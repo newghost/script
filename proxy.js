c58a7d85c2930f2336dad5d4faf951600249868c
Kris Zhang <c2u@live.cn>
2014-01-17 23:31:30 +0800

    proxy for debugging;

---
+++ proxy.js
@@ -0,0 +1,72 @@
+var fs    = require('fs')
+  , http  = require('http')
+  ;
+
+/*
+proxy
+*/
+var proxyMapping  = {
+    'ourjs.com'       : '127.0.0.1:8052'
+  , 'user.ourjs.com'  : '127.0.0.1:8053'
+  , 'api.ourjs.com'   : '127.0.0.1:8054'
+}
+
+var proxyHandler = function(proxyReq, proxyRes) {
+  var url       = proxyReq.url
+    , headers   = proxyReq.headers    
+    , hostname  = headers.host
+    , port      = 80
+    , path      = url.split(hostname)[1]
+    , method    = proxyReq.method
+    ;
+
+  var mapping = proxyMapping[hostname];
+
+  if (mapping) {
+    hostname  = mapping.split(':')[0] || hostname;
+    port      = mapping.split(':')[1] || port;
+  }
+
+  var reqBuffer = [];
+  proxyReq.on('data', function(chunk) {
+    console.log('reqBuffer');
+    reqBuffer.push(chunk);
+  });
+
+  proxyReq.on('end', function() {
+    var options = {
+        hostname: hostname
+      , port: port
+      , path: path
+      , method: method
+    };
+
+    console.log(options);
+
+    var req = http.request(options, function(res) {
+      //res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        proxyRes.write(chunk);
+      });
+      res.on('end', function() {
+        proxyRes.end();
+      });
+    });
+
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+
+    // write data to request body
+    req.write(Buffer.concat(reqBuffer));
+    req.end();
+  });
+
+  proxyReq.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+};
+
+
+var httpSvr = http.createServer(proxyHandler);
+httpSvr.listen(8080);
